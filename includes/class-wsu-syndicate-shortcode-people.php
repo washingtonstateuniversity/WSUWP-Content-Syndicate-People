d2418b7257ca2796f7e90e15998cc91e19e3057c
danialbleile <danialbleile@wsu.edu>
2017-10-04 15:56:52 -0700

    Fix white space error

--- includes/class-wsu-syndicate-shortcode-people.php
+++ includes/class-wsu-syndicate-shortcode-people.php
@@ -81,7 +81,7 @@ class WSU_Syndicate_Shortcode_People extends WSU_Syndicate_Shortcode_Base {
 
 		$people = $this->sort_items( $people, $atts );
 
-		// html of profiles that's filtered before adding to content. 
+		// html of profiles that's filtered before adding to content.
 		$inner_content = '';
 
 		foreach ( $people as $person ) {
